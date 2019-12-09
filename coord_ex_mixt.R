90ace527be90be31ac80360d699404a89ffca01f
Mario Becerra <mbecerracontreras@gmail.com>
2019-12-09 15:47:38 +0100

    First commit

---
+++ coord_ex_mixt.R
@@ -0,0 +1,158 @@
+# Coordinate exchange for mixtures
+# Based on section 6.3.5 of Goos and Jones
+# and Piepel, Cooley and Jones (2005)
+
+
+create_random_initial_design = function(n_runs, q, seed = NULL){
+  X = matrix(rep(NA_real_, n_runs*q), nrow = n_runs)
+  
+  if(!is.null(seed)) set.seed(seed)
+  
+  for(i in 1:nrow(X)){
+    rands = runif(q)
+    
+    # rows in X sum to 1:
+    X[i,] = rands/sum(rands)
+  }
+  
+  return(X)
+}
+
+
+compute_cox_direction = function(x, comp, n_points = 11){
+  
+  i = comp
+  q = length(x)
+  
+  
+  # points in Cox's direction
+  seq_points = seq(from = 0, to = 1, length.out = n_points)
+  
+  diffs = seq_points - x[i]
+  ix1 = which.min(abs(diffs))
+  
+  cox_direction_aux = seq_points - diffs[ix1]
+  cox_direction_aux2 = cox_direction_aux[-ix1]
+  betw_0_1 = cox_direction_aux2 >= 0 & cox_direction_aux2 <= 1
+  cox_direction_aux3 = c(0, cox_direction_aux2[betw_0_1], 1)
+  
+  cox_direction = matrix(rep(NA_real_, q*length(cox_direction_aux3)), ncol = q)
+  cox_direction[,i] = cox_direction_aux3
+  deltas = cox_direction_aux3 - x[i]
+  
+  for(n in seq_along(cox_direction_aux3)){
+    # recompute proportions:
+    for(j in setdiff(1:q, i)){
+      cox_direction[n, j] = x[j] - deltas[n]*x[j]/(1 - x[i])
+    }
+  }
+  
+  return(cox_direction)
+}
+
+
+
+plot_cox_direction = function(x_in, comp = NULL){
+  # x_in: vector of length 3 that sums up to 1
+  # comp: which component should be plotted. Can be a scalar or a vector
+  
+  if(length(x_in) != 3) stop("x_in must be of length 3")
+  if(sum(x_in) != 1) stop("x_in must sum up to 1")
+  
+  # If component is null
+  if(!is.null(comp) & length(comp) == 1){
+    out = compute_cox_direction(x_in, comp, 3) %>% 
+      as_tibble() %>% 
+      set_names(c("x", "y", "z")) %>% 
+      ggtern(aes(x, y, z)) + 
+      geom_path(linetype = "dashed") + 
+      theme_bw() +
+      geom_point(data = tibble(x = x_in[1], y = x_in[2], z = x_in[3]))
+  } else{
+    if(is.null(comp)) comp = 1:length(x_in)
+    
+    cox_dirs = lapply(1:length(comp), function(i){
+      compute_cox_direction(x_in, i, 3) %>% 
+        as_tibble() %>% 
+        set_names(c("x", "y", "z")) %>% 
+        mutate(comp = i)
+    }) %>% 
+      bind_rows()
+    
+    out = cox_dirs %>% 
+      ggtern(aes(x, y, z)) + 
+      geom_path(linetype = "dashed", aes(group = comp)) + 
+      theme_bw() +
+      geom_point(data = tibble(x = x_in[1], y = x_in[2], z = x_in[3]))
+  }
+  
+  return(out)
+}
+
+
+x_in = c(0.4725440, 0.2627838, 0.2646722)
+plot_cox_direction(x_in, 1)
+plot_cox_direction(x_in, 1:3)
+
+
+coord_ex_mixt = function(n_runs, q = 3, n_cox_points = 100, seed = NULL, X = NULL){
+  
+  if(is.null(X)){
+    # If no initial design is provided, create a random starting design
+    X = create_random_initial_design(n_runs, q, seed)
+    
+  } else{
+    # Check that rows in X sum to 1
+    row_sums = apply(X, 1, sum)
+    if(sum(row_sums == rep(1, n_runs)) != n_runs){
+      stop("Rows in X must sum 1")
+    }
+  }
+  
+  if(!is.null(seed)) set.seed(seed)
+  # Coordinate exchanges:
+  
+  for(k in 1:nrow(X)){
+    for(i in 1:q){
+      
+      # get points from Cox's direction
+      cox_direction = compute_cox_direction(X[k,], i, n_cox_points)
+      
+      # compute optimality criterion for points in Cox's direction
+      
+      
+      
+      
+    }
+    
+  }
+  
+  # for(k in 1:nrow(X)){
+  #   # Decide which column to change
+  #   i = sample(1:q, 1)
+  #   
+  #   # new value of xi:
+  #   x_new = runif(1)
+  #   
+  #   # Difference of new and old:
+  #   delta = x_new - X[k, i]
+  #   
+  #   # recompute proportions:
+  #   for(j in setdiff(1:q, i)){
+  #     X[k, j] = X[k, j] - delta*X[k, j]/(1 - X[k, i])
+  #   }
+  #   
+  #   # Change value of xi
+  #   X[k, i] = x_new
+  # }
+  
+  # The algorithm then evaluates the optimality criterion for a certain number of different designs, say k, obtained by replacing the original coordinate with k equidistant points on the Cox-effect direction line between the lower and upper limit.
+  
+}
+
+
+
+
+
+
+
