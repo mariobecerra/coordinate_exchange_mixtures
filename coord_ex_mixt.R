1aed7f6eb875af69c173394e6a84f99cdd05c0b9
Mario Becerra <mbecerracontreras@gmail.com>
2019-12-09 20:08:34 +0100

    Add libraries and show several ternary plots with Cox direction in random experiments

--- coord_ex_mixt.R
+++ coord_ex_mixt.R
@@ -55,6 +55,10 @@ compute_cox_direction = function(x, comp, n_points = 11){
 plot_cox_direction = function(x_in, comp = NULL){
   # x_in: vector of length 3 that sums up to 1
   # comp: which component should be plotted. Can be a scalar or a vector
+  library(dplyr)
+  library(purrr)
+  library(ggplot2)
+  library(ggtern)
   
   if(length(x_in) != 3) stop("x_in must be of length 3")
   if(sum(x_in) != 1) stop("x_in must sum up to 1")
@@ -64,7 +68,8 @@ plot_cox_direction = function(x_in, comp = NULL){
     out = compute_cox_direction(x_in, comp, 3) %>% 
       as_tibble() %>% 
       set_names(c("x", "y", "z")) %>% 
-      ggtern(aes(x, y, z)) + 
+      ggplot(aes(x, y, z)) +
+      coord_tern() + 
       geom_path(linetype = "dashed") + 
       theme_bw() +
       geom_point(data = tibble(x = x_in[1], y = x_in[2], z = x_in[3]))
@@ -79,8 +84,15 @@ plot_cox_direction = function(x_in, comp = NULL){
     }) %>% 
       bind_rows()
     
+    # out = cox_dirs %>% 
+    #   ggtern(aes(x, y, z)) + 
+    #   geom_path(linetype = "dashed", aes(group = comp)) + 
+    #   theme_bw() +
+    #   geom_point(data = tibble(x = x_in[1], y = x_in[2], z = x_in[3]))
+    
     out = cox_dirs %>% 
-      ggtern(aes(x, y, z)) + 
+      ggplot(aes(x, y, z)) +
+      coord_tern() + 
       geom_path(linetype = "dashed", aes(group = comp)) + 
       theme_bw() +
       geom_point(data = tibble(x = x_in[1], y = x_in[2], z = x_in[3]))
@@ -89,10 +101,26 @@ plot_cox_direction = function(x_in, comp = NULL){
   return(out)
 }
 
+library(tidyverse)
+library(ggtern)
 
 x_in = c(0.4725440, 0.2627838, 0.2646722)
 plot_cox_direction(x_in, 1)
 plot_cox_direction(x_in, 1:3)
+
+
+# plot several Cox's directions for random designs
+random_designs = create_random_initial_design(8, q = 3)
+
+cox_direction_plots = lapply(1:nrow(random_designs), function(i){
+  return(plot_cox_direction(random_designs[i,], 1:3))
+})
+
+# Don't understand why the plots are not right when arranged in a grid with gridExtra
+# I need to use ggtern's own grid.arrange
+# https://stackoverflow.com/questions/42825983/issue-when-using-grid-arrange-with-ggtern-generated-plots
+# do.call("grid.arrange", c(cox_direction_plots, ncol = 4)) # This works with ggtern::grid.arrange
+ggtern::grid.arrange(grobs = cox_direction_plots, ncol = 4)
 
 
 coord_ex_mixt = function(n_runs, q = 3, n_cox_points = 100, seed = NULL, X = NULL){
